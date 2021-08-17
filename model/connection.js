2aba19ce63658de4509e85b4230dcfc70fca2e6f
dominikuu <alanccl92@hotmail.com>
2021-08-17 13:26:26 +0800

    remove redudant

--- model/connection.js
+++ model/connection.js
@@ -1,10 +1,6 @@
 const knex = require('knex')({
     client: 'pg',
     connection: process.env.POSTGRES_URI,
-    // pool: {
-    //   min: 0,
-    //   max: 7
-    // }
 });
     
 module.exports = knex;
--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const morgan = require("morgan");
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image");
-const auth = require("./controllers/authorization");
 const tinyUrl = require("./controllers/tinyUrl");
 
 //Database Setup
@@ -20,8 +17,6 @@ const db = knex({
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const whitelist = ["http://localhost:3000"];
 const corsOptions = {
   origin: function (origin, callback) {
     console.log("origin=>>>>", origin);
@@ -41,22 +36,9 @@ app.post("/signin", signin.signinAuthentication(db, bcrypt));
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
-app.get("/profile/:id", auth.requireAuth, (req, res) => {
-  profile.handleProfileGet(req, res, db);
-});
-app.post("/profile/:id", auth.requireAuth, (req, res) => {
-  profile.handleProfileUpdate(req, res, db);
-});
-app.put("/image", auth.requireAuth, (req, res) => {
-  image.handleImage(req, res, db);
-});
-app.post("/imageurl", auth.requireAuth, (req, res) => {
-  image.handleApiCall(req, res);
-});
 app.get("/shortenurl/:url", async (req, res) => {
   tinyUrl.handleUrlRedirect(req, res, db);
 });
-// app.post('/shortenurl', auth.requireAuth, (req, res) => {tinyUrl.handleUrlShorten(req, res)})
 app.post("/shortenurl", async (req, res) => {
   tinyUrl.handleUrlShorten(req, res, db);
 });
